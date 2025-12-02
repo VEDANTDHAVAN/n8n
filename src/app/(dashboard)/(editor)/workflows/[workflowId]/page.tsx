ef2e496506ba3568e2460fc9959155252656ba5f
Vedant <vedantadhavan11@gmail.com>
2025-12-02 11:17:21 +0530

    workflow page with edit name option

--- src/app/(dashboard)/(editor)/workflows/[workflowId]/page.tsx
+++ src/app/(dashboard)/(editor)/workflows/[workflowId]/page.tsx
@@ -1,4 +1,10 @@
+import { Editor, EditorError, EditorLoading } from "@/features/editor/components/editor";
+import { EditorHeader } from "@/features/editor/components/editor-header";
+import { WorkflowsList } from "@/features/workflows/components/workflows";
 import { requireAuth } from "@/lib/auth-utils";
+import { HydrateClient } from "@/trpc/server";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 
 interface PageProps {
     params: Promise<{
@@ -12,7 +18,16 @@ const Page = async ({ params }: PageProps) => {
     const { workflowId } = await params;
     
     return (
-        <p>Workflow ID: {workflowId}</p>
+     <HydrateClient>
+        <ErrorBoundary fallback={<EditorError />}> 
+         <Suspense fallback={<EditorLoading />}>
+          <EditorHeader workflowId={workflowId} />
+          <main className="flex-1">
+            <Editor workflowId={workflowId} />
+          </main>
+         </Suspense>
+        </ErrorBoundary>
+     </HydrateClient>
     );
 };
 
